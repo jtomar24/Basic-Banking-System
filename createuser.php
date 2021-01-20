b0e3a5d1eb66ff46737c4ebc3ec015b250c0113f
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-01-20 23:39:07 +0530

    Add files via upload

---
+++ createuser.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+    <style>
+       .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+   
+ footer {
+      background-color:#17a2ba;
+      padding: 25px;
+    }
+
+
+
+    </style>
+</head>
+
+<body style="background-color : #F2F3F4 ;">
+  
+
+
+<?php
+    include 'config.php';
+    if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $balance=$_POST['balance'];
+    $sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
+    $result=mysqli_query($conn,$sql);
+    if($result){
+               echo "<script> alert('Hurray! User created');
+                               window.location='transfermoney.php';
+
+                     </script>";
+                    
+    }
+  }
+?>
+
+<?php
+  include 'navbar.php';
+?>
+
+        <h2 class="text-center pt-4" style="color : black;">Create an User</h2>
+        <br>
+
+  <div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item text-center">
+          <img class="img-fluid" src="img/ulogin.png" style="border: none; border-radius: 50%;; ">
+        </div>
+        <div class="screen-body-item">
+          <form class="app-form" method="post">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME" type="text" name="name" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="BALANCE" type="number" name="balance" required>
+            </div>
+            <br>
+            <div class="app-form-group button">
+              <input class="app-form-button" type="submit" value="CREATE" name="submit"></input>
+              <input class="app-form-button" type="reset" value="RESET" name="reset"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+ 
+<footer class="container-fluid text-center">
+  <p> <div class="container">Copyright &copy; 2021 Credit Management by <i>Juhi Tomar</i>   
+
+    </div></p>
+</footer>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-sm bg-info navbar-light">
+
+  <a class="navbar-brand" href="#">QUIZY BANK</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="createuser.php">CREATE USER</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transfermoney.php">TRANSFER MONEY</a>
+      </li>  
+      <li class="nav-item">
+        <a class="nav-link" href="transactionhistory.php">TRANSACTION HISTORY</a>
+      </li>   
+    </ul>
+  </div>  
+</nav>
+<br>
+
+
+
+</body>
+</html>
